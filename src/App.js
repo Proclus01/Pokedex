9b818cbc2110f8733163a18e1f9c86dd6c6bfd74
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-29 00:17:43 -0700

    refactor pokedex into pokegame component and move array into pokegame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,20 +1,10 @@
 import Pokecard from "./Pokecard";
 
-const pokemonArray = [
-    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-    {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-    {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-    {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-    {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-    {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-    {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-    {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-  ];
+function Pokedex(props) {
 
-function Pokedex() {
     return (
         <div>
-            {pokemonArray.map(
+            {props.pokemonArray.map(
                 item => 
                     <Pokecard key={item.name}
                         id={item.id} 
