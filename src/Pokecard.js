e43ea29cd63404a797d23c9846065ed2f13d21ce
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-28 23:28:37 -0700

    create sample card and accompanying css

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,3 +1,5 @@
+import './Pokecard.css';
+
 function Pokecard(props) {
     const pokemon = props.pokemonArray;
 
@@ -7,9 +9,24 @@ function Pokecard(props) {
       }
     
     const testId = prepareId(pokemon[0].id);
+    const testName = prepareId(pokemon[0].name);
+    const testType = prepareId(pokemon[0].type);
+    const testExp = prepareId(pokemon[0].base_experience);
 
     return (
-        <p>{testId}</p>
+        <div className="card-container">
+
+            <div className="image-container">
+                <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${testId}.png`} />
+            </div>
+
+            <div className="card-body">
+                <h2>{testName}</h2>
+                <p>Type: {testType}</p>
+                <p>Exp: {testExp}</p>
+            </div>
+
+        </div>
     );
 }
 
